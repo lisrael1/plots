5cb80143a45315d24e6009dd0bfb07bd7991ff07
lisrael1 <israelilior@gmail.com>
2018-07-10 12:26:46 +0300

    Update multi_sine.py

--- multi_sine.py
+++ multi_sine.py
@@ -9,7 +9,9 @@ amp=np.random.normal(0.2,0.01,sines)
 offset=np.random.normal(0,0.05,sines)
 phase=np.random.normal(0,3,sines)
 freq=np.random.normal(200,100,sines)
+y=[amp[i]*np.sin(2*np.pi*x*freq[i]+phase[i])+offset[i] for i in range(sines)]
 
-data = [dict(x=x, y=amp[i]*np.sin(2*np.pi*x*freq[i]+phase[i])+offset[i], name='sine %d'%i, type='scatter',mode='lines') for i in range(len(amp))]
-layout=cufflinks.getLayout(title='hi',xTitle="my x",yTitle='my y')
+data = [dict(x=x, y=y[i], name='sine %d'%i, type='scatter',mode='lines') for i in range(sines)]
+layout=cufflinks.getLayout(title='hello world',xTitle="my x",yTitle='my y')
 py.offline.plot(dict(data=data,layout=layout))
+
